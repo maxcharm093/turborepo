9c703a297b1a8b6269814c8ee85a00fc543ad565
Aung Pyae Phyo <aungphyo.dev@gmail.com>
2025-03-08 10:34:50 +0630

    feat: commitlint cli added in package.json

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,1 +1,22 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf',
+        'vercel',
+      ],
+    ],
+  },
+};
